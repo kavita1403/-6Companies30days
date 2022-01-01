1e431ae548ebbb3e21f125e2466b7ee7e4a84e47
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-01 22:21:29 +0530

    Update and rename 1st Ques.cpp.txt to 1st Ques.cpp

---
+++ Goldman Sachs/1st Ques.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+vector<string> ans,ansRev;
+std::map<string,vector<string>> v,revV;
+map<string,int> visited;
+
+
+// Time Complexicity: O(n+e)
+void solve()
+{
+  
+    
+}
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+  freopen("inp.txt" , "r" , stdin);
+  freopen("op3.txt" , "w" , stdout);
+#endif
+  int t = 1;
+  cin>>t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
