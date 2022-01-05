4ead803755cc19a19d71a6f8385112c0b7651e1d
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-05 23:33:14 +0530

    Update 9-Number following a pattern.cpp

--- Goldman Sachs/9-Number following a pattern.cpp
+++ Goldman Sachs/9-Number following a pattern.cpp
@@ -1,1 +1,29 @@
-
+string printMinNumberForPattern(string s){
+        int x=1;
+        string st="";
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='I')
+            {
+                st+=to_string(x++);
+            }
+            else
+            {
+                int cnt=0;
+                while(i<s.size()&& s[i]=='D')
+                {
+                    i++;
+                    cnt++;
+                }
+                    cnt++;
+                for(int j=x+cnt-1;j>=x;j--)
+                {
+                    st+=to_string(j);
+                }
+                x+=cnt;
+            }
+        }
+        if(s[s.size()-1]=='I')
+        st+=to_string(x);
+    return st;
+    }
