da2ebdae7b617d11cfddc9c2d516686f464ca966
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-10 13:25:19 +0530

    Update 3-IPL 2021.cpp

--- AMAZON/3-IPL 2021.cpp
+++ AMAZON/3-IPL 2021.cpp
@@ -1,24 +1,23 @@
-class Solution
-{
-    public:
-    void linkdelete(struct Node  *head, int M, int N)
-    {
-  
-      
-         Node *start=head;
-        while(start){
-           
-            for(int i=1;i< M && start;i++){
-                start=start->next;
+class Solution {
+  public:
+    vector<int> max_of_subarrays(vector<int> nums, int n, int k) {
+          
+        int i = 0, j = 0, sz = n ;
+        vector<int> res; deque<int> dq ;
+        
+        while(j<sz)
+        {
+            while(!dq.empty() && nums[j] > dq.back()){ dq.pop_back() ;}
+            dq.push_back(nums[j]) ;
+            
+            if(j-i+1 < k){ j++ ;}
+            else{
+                res.push_back(dq.front()) ;
+                if(nums[i] == dq.front()){ dq.pop_front() ;}
+                i++ ; j++ ;
             }
-            if(!start)return;
-            Node *temp =start;
-            for(int j=0;j<=N && temp;j++){
-                temp=temp->next;
-            }
-            start->next=temp;
-            start=temp;
-        } 
-    
+        }
+        
+        return res ;
     }
 };
