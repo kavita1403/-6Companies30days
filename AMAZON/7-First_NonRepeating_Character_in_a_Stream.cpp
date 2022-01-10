d318f1262d54e4a3810b55ef1304ca7bf0ee0565
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-10 13:26:16 +0530

    Update 7-First_NonRepeating_Character_in_a_Stream.cpp

--- AMAZON/7-First_NonRepeating_Character_in_a_Stream.cpp
+++ AMAZON/7-First_NonRepeating_Character_in_a_Stream.cpp
@@ -1,1 +1,21 @@
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		queue<char>q;
+		string ans="";
+		int freq[123]={0};
+		for(int i=0;i<A.size();i++){
+		    q.push(A[i]);
+		    freq[A[i]]++;
+		    while(!q.empty() && freq[q.front()]!=1) q.pop();
+		    if(q.size()!=0){
+		        ans +=q.front();
+		    }else{
+		        ans +='#';
+		    }
+		}
+		 return ans;
+		    
+		}
 
+};
