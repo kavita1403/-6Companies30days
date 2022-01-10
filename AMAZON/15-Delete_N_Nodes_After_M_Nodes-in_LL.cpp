19b2ab0b2f5fa39251847b994b50f272ebf98d27
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-10 13:22:44 +0530

    Update 15-Delete_N_Nodes_After_M_Nodes-in_LL.cpp

--- AMAZON/15-Delete_N_Nodes_After_M_Nodes-in_LL.cpp
+++ AMAZON/15-Delete_N_Nodes_After_M_Nodes-in_LL.cpp
@@ -1,1 +1,24 @@
-
+class Solution
+{
+    public:
+    void linkdelete(struct Node  *head, int M, int N)
+    {
+  
+      
+         Node *start=head;
+        while(start){
+           
+            for(int i=1;i< M && start;i++){
+                start=start->next;
+            }
+            if(!start)return;
+            Node *temp =start;
+            for(int j=0;j<=N && temp;j++){
+                temp=temp->next;
+            }
+            start->next=temp;
+            start=temp;
+        } 
+    
+    }
+};
