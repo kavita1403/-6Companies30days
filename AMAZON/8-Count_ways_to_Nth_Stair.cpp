8566a74a9d849b868652acf7e27445823db0c26c
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-10 14:02:32 +0530

    Update 8-Count_ways_to_Nth_Stair.cpp

--- AMAZON/8-Count_ways_to_Nth_Stair.cpp
+++ AMAZON/8-Count_ways_to_Nth_Stair.cpp
@@ -1,1 +1,15 @@
+Q Link: https://practice.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter1322/1/
+
+
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    long long countWays(int m)
+    {
+        // your code here
+        return long((m/2)+1);
+    }
+};
 
