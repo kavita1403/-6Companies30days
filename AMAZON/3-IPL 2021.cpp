5c54d8858aa54441c5cbf99e457a98e24e088b21
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-10 13:32:44 +0530

    Update 3-IPL 2021.cpp

--- AMAZON/3-IPL 2021.cpp
+++ AMAZON/3-IPL 2021.cpp
@@ -1,3 +1,6 @@
+Q Link: https://practice.geeksforgeeks.org/problems/deee0e8cf9910e7219f663c18d6d640ea0b87f87/1/
+
+
 class Solution {
   public:
     vector<int> max_of_subarrays(vector<int> nums, int n, int k) {
