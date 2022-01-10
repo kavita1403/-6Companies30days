33eadaa93961126172a03de016f4d848c72df2bb
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-10 13:29:06 +0530

    Update 12-Count_Name_From_Given_ColumnNumber.cpp

--- AMAZON/12-Count_Name_From_Given_ColumnNumber.cpp
+++ AMAZON/12-Count_Name_From_Given_ColumnNumber.cpp
@@ -1,1 +1,18 @@
+Q Link: https://practice.geeksforgeeks.org/problems/column-name-from-a-given-column-number4244/1/
 
+
+
+class Solution{
+    public:
+    string colName (long long int n)
+    {
+        string result="";
+        while(n){
+            char c='A'+(n-1)%26;
+            n=(n-1)/26;
+            result=c+result;
+        }
+       
+        return result;
+    }
+};
