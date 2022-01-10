60e2dbf69236748a0b204b1598ac566b1f63c0ea
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-10 13:27:53 +0530

    Update 10-Nuts & Bolts.cpp

--- AMAZON/10-Nuts & Bolts.cpp
+++ AMAZON/10-Nuts & Bolts.cpp
@@ -1,1 +1,33 @@
+*Have to optimize the solution*
+Q Link- https://practice.geeksforgeeks.org/problems/nuts-and-bolts-problem0431/1
 
+
+
+
+class Solution{
+public:	
+
+	void matchPairs(char nuts[], char bolts[], int n) {
+	   map<char,int>mp;
+	   
+	   for(int i=0;i<n;i++){
+	       mp[nuts[i]]++;
+	   }
+	   int k=0;
+	   for(auto i:mp){
+	       nuts[k++]=i.first;
+	      if(k==n){
+	          break;
+	      } 
+	   }
+	   int l=0;
+	   for(auto i:mp){
+	       bolts[l++]=i.first;
+	      if(l==n){
+	          break;
+	      } 
+	   }
+	   
+	}
+
+};
