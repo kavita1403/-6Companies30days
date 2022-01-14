335faa30ddae8d65c96df91394f152ef08797727
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-14 17:18:52 +0530

    Update 09-Count Number of SubTrees having given Sum.cpp

--- MicroSoft/09-Count Number of SubTrees having given Sum.cpp
+++ MicroSoft/09-Count Number of SubTrees having given Sum.cpp
@@ -1,1 +1,15 @@
-
+Q Link: https://practice.geeksforgeeks.org/problems/count-number-of-subtrees-having-given-sum/1/#
+Solution- 
+  
+  int Sum(Node* root, int X, int &val){
+    if(!root)return 0;
+    int s=Sum(root->left,X,val)+Sum(root->right,X,val)+root->data;
+    if(s==X)val++;
+    return s;
+}
+int countSubtreesWithSumX(Node* root, int X)
+{
+  	int val=0;
+  	Sum(root,X,val);
+  	return val;
+}
