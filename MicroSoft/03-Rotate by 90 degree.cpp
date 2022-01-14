2be9414770dcd9e2409b5181e09425da1f2613b8
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-14 17:15:04 +0530

    Update 03-Rotate by 90 degree.cpp

--- MicroSoft/03-Rotate by 90 degree.cpp
+++ MicroSoft/03-Rotate by 90 degree.cpp
@@ -1,1 +1,17 @@
+Q Link:  https://practice.geeksforgeeks.org/problems/rotate-by-90-degree0356/1/
 
+
+Solution- 
+  
+  void rotate(vector<vector<int> >& matrix)
+{   
+    int n=matrix[0].size();
+    for(int i=0;i<n;i++)
+    {reverse(matrix[i].begin(),matrix[i].end());}
+    
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            swap(matrix[i][j],matrix[j][i]);
+        }
+    }
+}
