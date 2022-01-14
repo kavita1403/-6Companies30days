ce27f4d7927989f8bbbd5fd538be5e9eeada69e0
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-14 17:15:50 +0530

    Update 05-Stock Span Problem.cpp

--- MicroSoft/05-Stock Span Problem.cpp
+++ MicroSoft/05-Stock Span Problem.cpp
@@ -1,1 +1,33 @@
-
+Q Link: https://practice.geeksforgeeks.org/problems/stock-span-problem-1587115621/1
+Solution- 
+  
+  class Solution
+{
+    public:
+    //Function to calculate the span of stockâ€™s price for all n days.
+    vector <int> calculateSpan(int price[], int n)
+    {
+    vector<int>v;
+    stack<pair<int,int>>s;
+    for(int i=0;i<n;i++){
+        if(s.size()==0){
+            v.push_back(-1);
+        }else if(s.size()>0 && s.top().first>price[i]){
+            v.push_back(s.top().second);
+        }else if(s.size()>0 && s.top().first<=price[i]){
+            while(s.size()>0 && s.top().first<=price[i]){
+                s.pop();
+            }if(s.size()==0){
+              v.push_back(-1);  
+            }else{
+              v.push_back(s.top().second);  
+            }
+        }
+        s.push({price[i],i});
+    }
+    for(int i=0;i<n;i++){
+        v[i]=i-v[i];
+    }
+    return v;
+    }
+};
