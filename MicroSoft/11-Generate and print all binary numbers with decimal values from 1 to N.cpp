e872f056ca2031950aa7c1a1545f5852a2c53b52
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-14 17:12:54 +0530

    Update 11-Generate and print all binary numbers with decimal values from 1 to N.cpp

--- MicroSoft/11-Generate and print all binary numbers with decimal values from 1 to N.cpp
+++ MicroSoft/11-Generate and print all binary numbers with decimal values from 1 to N.cpp
@@ -1,1 +1,21 @@
+Q Link- https://practice.geeksforgeeks.org/problems/generate-binary-numbers-1587115620/1/#
 
+
+Solution- 
+  
+  vector<string> generate(int N)
+{
+    vector<string>ans;
+    string res="";
+   for(int i=1;i<=N;i++){
+       int a=i;
+      while(a){
+          
+          res=char(a%2+'0')+res;
+          a/=2;
+      } 
+      ans.push_back(res);
+      res="";
+   }
+   return ans;
+}
