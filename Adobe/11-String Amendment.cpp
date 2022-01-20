3d18ad233041cc8c3facf2528a38d6c2f02e10a1
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-20 18:17:38 +0530

    Update 11-String Amendment.cpp

--- Adobe/11-String Amendment.cpp
+++ Adobe/11-String Amendment.cpp
@@ -1,1 +1,19 @@
-
+class Solution{
+    public:
+    string amendSentence (string s)
+    {
+        string str="";
+        str+=tolower(s[0]);
+        // int i=1;
+        for(int i=1;i<s.size();i++){
+            if(isupper(s[i])){
+                str+=" ";
+                str+=tolower(s[i]);
+            }
+            else{
+                str+=s[i];
+            }
+        }
+        return str;
+    }
+};
