cc8c93bb42c048bd14d6ae0ad668cb8da89e2505
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-20 18:16:34 +0530

    Update 10-Elections.cpp

--- Adobe/10-Elections.cpp
+++ Adobe/10-Elections.cpp
@@ -1,1 +1,28 @@
-
+class Solution{
+  public:
+  
+    //Function to return the name of candidate that received maximum votes.
+    vector<string> winner(string arr[],int n)
+    {
+        int maxi=INT_MIN;
+        string name="";
+        unordered_map<string,int>m;
+        for(int i=0;i<n;i++){
+            m[arr[i]]++;
+        }
+        for(auto i:m){
+            if(i.second>maxi){
+                maxi=i.second;
+                name=i.first;
+            }else if(i.second==maxi){
+                if(name<i.first){
+                    continue;
+                }else{
+                    name=i.first;
+                }
+            }
+        }
+      vector<string> vec = {name,to_string(maxi)}; 
+      return vec;
+    }
+};
