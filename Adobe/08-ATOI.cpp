76ad439c1bc0b732f8fd0eb40d18753d7466bb8a
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-20 18:15:34 +0530

    Update 08-ATOI.cpp

--- Adobe/08-ATOI.cpp
+++ Adobe/08-ATOI.cpp
@@ -1,1 +1,27 @@
-
+class Solution{
+  public:
+    /*You are required to complete this method */
+    int atoi(string str) {
+     
+        int ans=0;
+        int sign=1;
+        int i=0;
+        if(str[i]=='-'){
+            sign=-1;
+            i++;
+        }
+      for(;i<str.size();i++){
+          if(isdigit(str[i])){
+               ans =ans*10+(str[i]-'0'); 
+             
+              
+          }else{
+             return -1;
+          }
+      }  
+     return ans*sign;  
+     
+     
+  
+    }
+};
