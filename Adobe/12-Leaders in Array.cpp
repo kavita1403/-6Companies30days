915f2cc8843db48ddca35cf1aaa035a0ff2ca43d
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-20 19:47:07 +0530

    Update 12-Leaders in Array.cpp

--- Adobe/12-Leaders in Array.cpp
+++ Adobe/12-Leaders in Array.cpp
@@ -1,1 +1,27 @@
-
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int a[], int n){
+        // Code here
+     vector<int>v;
+     for(int i=n-1; i>=0;i--)
+     {
+         if(v.size()==0)
+         {
+             v.push_back(a[n-1]);
+         }
+         else if(a[i]>=v.back())
+         {
+             v.push_back(a[i]);
+         }
+        //  else
+        //  {
+        //      continue;
+        //  }
+     }
+     reverse(v.begin(),v.end());
+     return v;
+     
+     
+    }
+};
