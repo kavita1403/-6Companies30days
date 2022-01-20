312a7049f01ef5152c66b8c1c357b412d529f1c9
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-20 22:41:53 +0530

    Update 02-Length of longest Arithmetic Progression.cpp

--- Adobe/02-Length of longest Arithmetic Progression.cpp
+++ Adobe/02-Length of longest Arithmetic Progression.cpp
@@ -1,1 +1,23 @@
-
+int lengthOfLongestAP(int ar[], int n) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++)
+            mp[ar[i]]=1;
+        int mxlen=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int pre=ar[i];
+                int cur=ar[j];
+                int diff=cur-pre;
+                int cnt=1;
+                while(cur<=ar[n-1] && mp.find(cur)!=mp.end())
+                {
+                    cur+=diff;
+                    cnt++;
+                }
+                mxlen=max(cnt,mxlen);
+            }
+        }
+        return mxlen;
+    }
