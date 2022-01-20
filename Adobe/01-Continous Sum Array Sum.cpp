81e7674ceeff4ede6e5751f8f4c23406060a0ee2
Kavita Yadav <56086999+kavita1403@users.noreply.github.com>
2022-01-20 18:13:43 +0530

    Update 01-Continous Sum Array Sum.cpp

--- Adobe/01-Continous Sum Array Sum.cpp
+++ Adobe/01-Continous Sum Array Sum.cpp
@@ -1,1 +1,34 @@
-
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(int arr[], int n, long long s)
+    {
+        vector<int>v;
+        vector<int>v1={-1};
+       int i=0,j=0;
+       long long ans=0;
+       while(j<n){
+          ans+=arr[j];
+          if(ans<s){
+              j++;
+          }else if(ans==s){
+              v.push_back(i+1);
+              v.push_back(j+1);
+             return v;
+          }else if(ans>s){
+              while(ans>s){
+                  ans -=arr[i];
+                  i++;
+              }
+              if(ans==s){
+                  v.push_back(i+1);
+                   v.push_back(j+1);
+                   return v;
+              }
+              j++;
+          }
+       }
+       return v1;
+    }
+};
